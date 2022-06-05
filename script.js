e3ee5760aed6353d89839d547688dfe4107dae24
cangnidepigu <finalandeventual@gmail.com>
2022-06-05 23:20:34 +0200

    3D object in the middle

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-dragElement(document.getElementById('plant1'));
-dragElement(document.getElementById('plant2'));
-dragElement(document.getElementById('plant3'));
-dragElement(document.getElementById('plant4'));
-dragElement(document.getElementById('plant5'));
-dragElement(document.getElementById('plant6'));
-dragElement(document.getElementById('plant7'));
-dragElement(document.getElementById('plant8'));
-dragElement(document.getElementById('plant9'));
-dragElement(document.getElementById('plant10'));
-dragElement(document.getElementById('plant11'));
-dragElement(document.getElementById('plant12'));
-dragElement(document.getElementById('plant13'));
-dragElement(document.getElementById('plant14'));
+import * as THREE from "../node_modules/three/build/three.module.js";
 
+// Scene
+const scene = new THREE.Scene();
 
-function dragElement(terrariumElement) {
-    // set 4 positions for positionins on the screen
-    let pos1 = 0,
-        pos2 = 0,
-        pos3 = 0,
-        pos4 = 0;
-    terrariumElement.onpointerdown = pointerDrag;
-    function pointerDrag(e) {
-        e.preventDefault();
-        console.log(e);
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onpointermove = elementDrag;
-        document.onpointerup = stopElementDrag;
-    } function elementDrag(e) {
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        console.log(pos1, pos2, pos3, pos4);
-        terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
-        terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
-    } function stopElementDrag() {
-        document.onpointerup = null;
-        document.onpointermove = null;
-    }
-}
+// Add a cube to the scene
+const geometry = new THREE.BoxGeometry(3, 1, 3); // width, height, depth
+const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0, 0, 0);
+scene.add(mesh);
+
+// Set up lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight.position.set(10, 20, 0); // x, y, z
+scene.add(directionalLight);
+
+// Camera
+const width = 10;
+const height = width * (window.innerHeight / window.innerWidth);
+const camera = new THREE.OrthographicCamera(
+  width / -2, // left
+  width / 2, // right
+  height / 2, // top
+  height / -2, // bottom
+  1, // near
+  100 // far
+);
+
+camera.position.set(4, 4, 4);
+camera.lookAt(0, 0, 0);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+// Add it to HTML
+document.body.appendChild(renderer.domElement);
